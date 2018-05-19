bc85e8d9ab38e57e498467ff239b3aa240529a40
Johan Lindgren <johan.lindgren@alphadev.se>
2018-05-19 14:59:38 +0200

    fixed eslint errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const App = () => ({
   render() {
     return (
       <div className="App">
@@ -15,7 +15,7 @@ class App extends Component {
         </p>
       </div>
     );
-  }
-}
+  },
+});
 
 export default App;
